428b09794f7c72e0de4fb899a65d91ea29afc43d
Catherine Gilbert <catherine.gilbert15@gmail.com>
2022-09-27 22:11:35 -0400

    Ajout de contenu pour le bras robotisé

---
+++ components/content/youtube-embed.vue
@@ -0,0 +1,18 @@
+<template>
+  <iframe
+      :src="`https://www.youtube.com/embed/${props.videoId}`"
+      :title="props.title"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+      class="w-full aspect-video"
+  />
+</template>
+
+<script lang="ts" setup>
+interface YoutubeEmbedProps {
+  videoId: string,
+  title: string,
+}
+
+const props = defineProps<YoutubeEmbedProps>();
+</script>
--- content/projets/bras-robotise.md
+++
@@ -1,6 +0,0 @@
----
-title: 'Simulation bras robotisé'
-description: 'Apprendre à interagir avec un bras robotisé utilisé dans les usines sans danger'
----
-
-# Simulation bras robotisé
