59039d6e1df1baf9644a8dae89cb3d8176dd3218
Catherine Gilbert <catherine.gilbert15@gmail.com>
2022-09-30 22:20:26 -0400

    Amélioration de la page listant les projets

--- pages/projets/index.vue
+++ pages/projets/index.vue
@@ -1,16 +1,27 @@
 <template>
   <div>
-    <h1>Projets</h1>
+    <div class="prose">
+      <h1>Projets</h1>
+      <p>Vous trouverez ici une liste de mes projets personnels</p>
+    </div>
     <ContentList v-slot="{ list }" path="/projets">
-      <a v-for="project in list" :key="project._path" :href="project._path">
-        <h2>{{ project.title }}</h2>
-        <p>{{ project.description }}</p>
+      <a
+          v-for="project in list"
+          :key="project._path"
+          :href="project._path"
+          class="flex align-middle justify-between bg-neutral mt-4 p-4 rounded-xl">
+        <div>
+          <h2 class="text-lg font-bold">{{ project.title }}</h2>
+          <p>{{ project.description }}</p>
+        </div>
+        <ChevronRightIcon class="place-self-center shrink-0 ml-4 w-8"/>
       </a>
     </ContentList>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ChevronRightIcon } from "@heroicons/vue/24/solid/index.js"
 import { definePageMeta } from "#imports";
 
 definePageMeta({
