5135be048b8e1f7390fb1ac8b7d98e92ea18b3bd
Catherine Gilbert <catherine.gilbert15@gmail.com>
2023-11-03 20:32:04 -0400

    Ajout prettier et eslint

--- src/i18n/messages.ts
+++ src/i18n/messages.ts
@@ -1,17 +1,17 @@
 export const langues = {
-    fr: "Français",
-    en: "English",
-}
+  fr: "Français",
+  en: "English",
+};
 
-export const langueParDefaut = "fr"
+export const langueParDefaut = "fr";
 
 export const messages = {
-    fr: {
-        "accueil.titre": "Catherine Gilbert",
-        "accueil.description": "À propos de Catherine Gilbert"
-    },
-    en: {
-        "accueil.titre": "Catherine Gilbert",
-        "accueil.description": "About Catherine Gilbert"
-    },
-}
+  fr: {
+    "accueil.titre": "Catherine Gilbert",
+    "accueil.description": "À propos de Catherine Gilbert",
+  },
+  en: {
+    "accueil.titre": "Catherine Gilbert",
+    "accueil.description": "About Catherine Gilbert",
+  },
+};
--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,19 +1,19 @@
-import {langueParDefaut, messages} from "./messages.ts";
+import { langueParDefaut, messages } from "./messages.ts";
 
 export function obtenirLangueSelonUrl(url: URL) {
-    const [, langue] = url.pathname.split("/");
-    if (langue && langue in messages) return langue as keyof typeof messages;
-    return langueParDefaut;
+  const [, langue] = url.pathname.split("/");
+  if (langue && langue in messages) return langue as keyof typeof messages;
+  return langueParDefaut;
 }
 
 export function utiliserTraductions(language: keyof typeof messages) {
-    return function t(key: keyof typeof messages[typeof langueParDefaut]) {
-        return messages[language][key] || messages[langueParDefaut][key];
-    }
+  return function t(key: keyof (typeof messages)[typeof langueParDefaut]) {
+    return messages[language][key] || messages[langueParDefaut][key];
+  };
 }
 
 export function utiliserCheminTraduit(language: keyof typeof messages) {
-    return function traduireChemin(path: string, l: string = language) {
-        return l === langueParDefaut ? path : `/${l}${path}`
-    }
+  return function traduireChemin(path: string, l: string = language) {
+    return l === langueParDefaut ? path : `/${l}${path}`;
+  };
 }
