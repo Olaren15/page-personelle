d86b27270a039779a871c8002795ef14be28ec03
Catherine Gilbert <catherine.gilbert15@gmail.com>
2023-11-04 23:39:57 -0400

    Ajout d'un hero sur la page d'accueil

--- src/i18n/messages.ts
+++ src/i18n/messages.ts
@@ -9,9 +9,15 @@ export const messages = {
   fr: {
     "accueil.titre": "Catherine Gilbert",
     "accueil.description": "À propos de Catherine Gilbert",
+    "accueil.hero.nom": "Catherine Gilbert",
+    "accueil.hero.poste": "Développeuse full-stack",
+    "accueil.hero.photo.alt": "Une photo de moi",
   },
   en: {
     "accueil.titre": "Catherine Gilbert",
     "accueil.description": "About Catherine Gilbert",
+    "accueil.hero.nom": "Catherine Gilbert",
+    "accueil.hero.poste": "Full-Stack Developer",
+    "accueil.hero.photo.alt": "A picture of me",
   },
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,17 @@ export default {
         sans: ["Zen Kaku Gothic New", ...defaultTheme.fontFamily.sans],
       },
     },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      "light-background": "#F5F4F2",
+      "light-foreground": "#131322",
+      "light-accent": "#BD92AD",
+      "dark-background": "#2c2c39",
+      "dark-foreground": "#FFFFFF",
+      "dark-accent": "#786A96",
+      main: "#8B83B8",
+    },
   },
   plugins: [],
 } satisfies Config;
