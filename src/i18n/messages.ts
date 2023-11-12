9ce01061bffeeb84e0da6619a6ed1bc565f03113
Catherine Gilbert <catherine.gilbert15@gmail.com>
2023-11-12 14:43:02 -0500

    Ajout pied de page

--- src/i18n/messages.ts
+++ src/i18n/messages.ts
@@ -15,7 +15,11 @@ export const messages = {
     "accueil.hero.nom": "Gilbert",
     "accueil.hero.poste": "Je suis une développeuse full-stack.",
     "accessibilite.navigation.contenu-principal": "Passer au contenu principal.",
+    "accessibilite.lien.ouvre-dans-un-nouvel-onglet": "Ce lien ouvrira dans un nouvel onglet.",
     "navigation.bascule-themme.description": "Activer le mode sombre.",
+    "pied-de-page.signature": "Fait avec ❤️ par une amie de Blåhaj",
+    "socials.github": "GitHub",
+    "socials.linkedIn": "LinkedIn",
   },
   en: {
     "ponctuation.point": ".",
@@ -26,6 +30,10 @@ export const messages = {
     "accueil.hero.nom": "Gilbert",
     "accueil.hero.poste": "I'm a Full-Stack Developer.",
     "accessibilite.navigation.contenu-principal": "Skip to main content",
+    "accessibilite.lien.ouvre-dans-un-nouvel-onglet": "This link will open in a new tab.",
     "navigation.bascule-themme.description": "Enable dark mode.",
+    "pied-de-page.signature": "Made with ❤️ by a friend of Blåhaj",
+    "socials.github": "GitHub",
+    "socials.linkedIn": "LinkedIn",
   },
 };
