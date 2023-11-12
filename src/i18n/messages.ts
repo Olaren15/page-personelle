7dec7d40aed0a71c31f8879581a5f3eb77e13189
Catherine Gilbert <catherine.gilbert15@gmail.com>
2023-11-11 19:42:23 -0500

    Redesign haut de page accueil

--- src/i18n/messages.ts
+++ src/i18n/messages.ts
@@ -7,19 +7,23 @@ export const langueParDefaut = "fr";
 
 export const messages = {
   fr: {
+    "ponctuation.point": ".",
     "accueil.titre": "Catherine Gilbert",
     "accueil.description": "À propos de Catherine Gilbert",
-    "accueil.hero.nom": "Catherine Gilbert",
-    "accueil.hero.poste": "Développeuse full-stack",
-    "accueil.hero.photo.alt": "Une photo de moi",
+    "accueil.hero.salutation": "Bonjour, mon nom est",
+    "accueil.hero.prenom": "Catherine",
+    "accueil.hero.nom": "Gilbert",
+    "accueil.hero.poste": "Je suis une développeuse full-stack.",
     "navigation.bascule-themme.description": "Activer le mode sombre",
   },
   en: {
+    "ponctuation.point": ".",
     "accueil.titre": "Catherine Gilbert",
     "accueil.description": "About Catherine Gilbert",
-    "accueil.hero.nom": "Catherine Gilbert",
-    "accueil.hero.poste": "Full-Stack Developer",
-    "accueil.hero.photo.alt": "A picture of me",
+    "accueil.hero.salutation": "Hi my name is",
+    "accueil.hero.prenom": "Catherine",
+    "accueil.hero.nom": "Gilbert",
+    "accueil.hero.poste": "I'm a Full-Stack Developer.",
     "navigation.bascule-themme.description": "Enable dark mode",
   },
 };
