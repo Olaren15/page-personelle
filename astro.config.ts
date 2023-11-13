619dc5a17c9e33164c06c8e2d095427d4813ea75
Catherine Gilbert <catherine.gilbert15@gmail.com>
2023-11-12 21:18:04 -0500

    Modification adresse site (#16)
    * Modification site dans configuration astro
    
    * Modification robots.txt
    
    * Ajustement README.md

--- astro.config.ts
+++ astro.config.ts
@@ -6,7 +6,7 @@ import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://olaren.dev",
+  site: "https://www.olaren.dev",
   integrations: [
     tailwind(),
     sitemap({
