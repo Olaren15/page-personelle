a62b6a8808e4da15fe3ea16151e30e995b7e7684
Catherine Gilbert <catherine.gilbert15@gmail.com>
2023-11-20 17:00:48 -0500

    Ajustement imports

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,4 +1,6 @@
-import { ImportMetaEnv } from "astro/client";
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="../.astro/types.d.ts" />
+import type { ImportMetaEnv } from "astro/client";
 
 interface ImportMetaEnv {
   readonly ENABLE_ANALYTICS: boolean;
